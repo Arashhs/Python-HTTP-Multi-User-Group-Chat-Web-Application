90c9325c646ddd4db3c5e724145ad93f8646dbb7
Arash Hajisafi <34424672+Arashhs@users.noreply.github.com>
2020-11-14 03:00:37 +0330

    Multi-threaded server is implemented. Clients work fine. System lock used to prevent race condition. Everything works GREAT!

--- client.py
+++ client.py
@@ -1,13 +1,34 @@
 #!/usr/bin/env python3
 import http.client
 import atexit
-
+import threading, json
 
 HOST_NAME='localhost'
 PORT_NUMBER=8000
+TIMEOUT = 10000
 token=""
 conn=None
 headers={}
+
+
+def recMessages(userName):
+    while True:
+        rcon = http.client.HTTPConnection(HOST_NAME, PORT_NUMBER)
+        rcon.request("GET", "", headers=headers)
+        while True:
+            try:
+                response = rcon.getresponse()
+                recMesJson = response.read().decode('utf-8')
+                recMes = json.loads(recMesJson)
+                if (recMes['sender'] != userName and recMes != {}):
+                    print('{}: {}'.format(recMes['sender'], recMes['body']))
+                break
+            except:
+                pass
+        # response = rcon.getresponse()
+        # recMes = response.read().decode('utf-8')
+        # print(recMes)
+
 
 
 def main():
@@ -32,6 +53,9 @@ def main():
     print('You joined the chat. Your auth is: {}'.format(token))
     # print(response.status, response.reason, response.read())
     atexit.register(exit_handler)
+    recThread = threading.Thread(target=recMessages, args=(userName,))
+    recThread.daemon = True
+    recThread.start()
     while True:
         message = input()
         conn.request("POST", "", message, headers=headers)
@@ -39,10 +63,11 @@ def main():
 
 
 
+
 def exit_handler():
     global headers
     conn.request("POST", "\quit", headers=headers)
-    print('My application is ending!')
+    print("You're leving the chat, bye!")
 
 
 
--- server.py
+++ server.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
 from http.server import HTTPServer, BaseHTTPRequestHandler
-import random, string
+import random, string, threading
+from socketserver import ThreadingMixIn
+import json, time
+
 
 
 HOST_NAME='localhost'
 PORT_NUMBER=8000
 TOKEN_LENGTH=12
+messageQueue = {}
+num = 0
+lock = threading.Lock()
+
 
 def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
@@ -13,14 +20,52 @@ def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
 
 class MainHandler(BaseHTTPRequestHandler):
     users = {}
+    getThreads = {}
+    global messageQueue
+    global num
+
+    def getResp(self):
+        global messageQueue
+        global num
+
+        """     while True:
+            with lock:
+                userName = self.users[self.headers['token']]
+                if(num != 0):
+                    print("receiver is: {} | Message is: {}".format(userName, messageQueue))
+                    self.send_response(200)
+                    self.end_headers()
+                    jsMes = json.dumps(messageQueue)
+                    self.wfile.write(jsMes.encode(encoding='utf-8'))
+                    num -= 1
+                    break 
+        """
+        while True:
+            with lock:
+                mes = messageQueue
+            userName = self.users[self.headers['token']]
+            if(num != 0):
+                print("receiver is: {} | Message is: {}".format(userName, mes))
+                self.send_response(200)
+                self.end_headers()
+                jsMes = json.dumps(mes)
+                self.wfile.write(jsMes.encode(encoding='utf-8'))
+                num -= 1
+                break
 
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("username", "123dszfasdf")
-        self.end_headers()
-    #    self.wfile.write()
+        # self.send_response(200)
+        # self.send_header("username", "123dszfasdf")
+        # self.end_headers()
+        # self.wfile.write("hello Bak".encode('utf-8'))
+        # getThread = threading.Thread(target=self.getResp, args=self)
+        # getThread.daemon = True
+        # getThread.start()
+        self.getResp()
 
     def do_POST(self):
+        global num
+        global messageQueue 
         users = self.users
         length = int(self.headers['content-length'])
         body = self.rfile.read(length).decode('utf-8')
@@ -40,22 +85,27 @@ class MainHandler(BaseHTTPRequestHandler):
             users.pop(self.headers['token'], None)
             print(users)
         else:
+            while True:
+                with lock:
+                    if(num==0):
+                        break
+
             messageBody = body
             sender = users[self.headers['token']]
             sendMessage = {'sender':sender, 'body':messageBody}
             print(sendMessage)
+            num += len(users)
+            messageQueue = sendMessage
 
-
-
-
-
+class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
+    """Handle requests in a separate thread."""
 
 
 
 
 def main():
     print("Server is running: {}:{}".format(HOST_NAME, PORT_NUMBER))
-    server = HTTPServer(("0.0.0.0", 8000), MainHandler)
+    server = ThreadedHTTPServer(("0.0.0.0", 8000), MainHandler)
     server.serve_forever()
 
 
