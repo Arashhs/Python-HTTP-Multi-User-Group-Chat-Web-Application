f23dadef3e1e0c74aa5d10023560a828e50e19c7
Arash Hajisafi <34424672+Arashhs@users.noreply.github.com>
2020-11-13 22:02:16 +0330

    Server now receives all the messages

--- client.py
+++ client.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python3
 import http.client
+import atexit
+
 
 HOST_NAME='localhost'
 PORT_NUMBER=8000
+token=""
+conn=None
+headers={}
 
 
 def main():
+    global conn 
+    global token
+    global headers
+
     userName = input('Please Enter Your Name: ')
     conn = http.client.HTTPConnection(HOST_NAME, PORT_NUMBER)
     # conn.request("GET", "/")
@@ -16,9 +25,25 @@ def main():
     # conn.request("GET", "/")
     # r1 = conn.getresponse()
     # print(r1.getheader("username"))
-    conn.request("POST", "\join")
+    conn.request("POST", "\join", userName)
     response = conn.getresponse()
-    print(response.status, response.reason, response.read())
+    token = response.read().decode('utf-8')
+    headers = {'token':token}
+    print('You joined the chat. Your auth is: {}'.format(token))
+    # print(response.status, response.reason, response.read())
+    atexit.register(exit_handler)
+    while True:
+        message = input()
+        conn.request("POST", "", message, headers=headers)
+        response = conn.getresponse()
+
+
+
+def exit_handler():
+    global headers
+    conn.request("POST", "\quit", headers=headers)
+    print('My application is ending!')
+
 
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -12,6 +12,8 @@ def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
 
 
 class MainHandler(BaseHTTPRequestHandler):
+    users = {}
+
     def do_GET(self):
         self.send_response(200)
         self.send_header("username", "123dszfasdf")
@@ -19,15 +21,32 @@ class MainHandler(BaseHTTPRequestHandler):
     #    self.wfile.write()
 
     def do_POST(self):
+        users = self.users
         length = int(self.headers['content-length'])
-        body = self.rfile.read(length)
+        body = self.rfile.read(length).decode('utf-8')
+        print(body)
         self.send_response(200)
         self.end_headers()
         # self.wfile.write("Received POST request with body: " + body)
         if(self.path == '\join'):
             token = id_generator(TOKEN_LENGTH)
-            print(token)
-            self.wfile.write(b'token')
+            # print(token)
+            self.wfile.write(token.encode('utf-8'))
+            users[token] = body
+            print('User {} joined the chat.'.format(body))
+            print(users)
+        elif(self.path == "\quit"):
+            print('User {} left the chat.'.format(users[self.headers['token']]))
+            users.pop(self.headers['token'], None)
+            print(users)
+        else:
+            messageBody = body
+            sender = users[self.headers['token']]
+            sendMessage = {'sender':sender, 'body':messageBody}
+            print(sendMessage)
+
+
+
 
 
 
@@ -35,7 +54,7 @@ class MainHandler(BaseHTTPRequestHandler):
 
 
 def main():
-    print("Server: {}:{}".format(HOST_NAME, PORT_NUMBER))
+    print("Server is running: {}:{}".format(HOST_NAME, PORT_NUMBER))
     server = HTTPServer(("0.0.0.0", 8000), MainHandler)
     server.serve_forever()
 
